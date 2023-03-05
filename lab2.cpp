ed279afd965bb5ef75e49e04bd1636726f727d19
ILS0 <silru@internet.ru>
2023-03-05 05:21:31 +0000

    new version

--- lab2.cpp
+++ lab2.cpp
@@ -2,19 +2,21 @@
 #include <cmath>
 
 //поддержка double
-//#define LF_SUP
+#define LF_SUP
 #ifdef LF_SUP 
 #define TYPE double
 #define ABS fabs
+#define MAX fmax
 #else
 #define TYPE float
 #define ABS fabsf
+#define MAX fmaxf
 #endif
 
 
 int main(){
 
-    TYPE error {0.0},tol{-1e-6};
+    TYPE error {1.0},tol{1e-6};
     int iter_max{1000000},iter{0},n{128},m{128};
 
 
@@ -25,41 +27,52 @@ int main(){
         Anew[i] = new TYPE[n];
     }
 
-    A[0][0]=10;
-    A[0][m-1] = 20;
-    A[n-1][0]=30;
-    A[n-1][m-1]=20;
+    #pragma acc enter data copy(A [0:n] [0:m],error) create(Anew[n][m])
+        A[0][0]=10;
+        A[0][m-1] = 20;
+        A[n-1][0]=30;
+        A[n-1][m-1]=20;
+    
 
+        #pragma acc parallel loop present(A[0:n][0:m])
+        for (int i{1};i<n-1;++i){
+            A[0][i]=10+((A[0][m-1] - A[0][0])/ (n-1))*i;
+            A[i][0]=10+((A[n-1][0] - A[0][0])/ (n-1))*i;
+            A[n-1][i]=10+((A[n-1][0] - A[n-1][m-1])/ (n-1))*i;
+            A[i][m-1]=10+((A[n-1][m-1] - A[n-1][0])/ (n-1))*i;
+        }
+        
+        while (error > tol && iter < iter_max){
+            error = 0.0;
+            #pragma update device(error)
 
-
-    #pragma acc data copy(A [1:n] [1:m]) create(Anew[n][m])
-    while (error > tol && iter < iter_max){
-        error = 0.0;
-
-        #pragma acc parallel loop reduction(max : error)
-        for (int j {1}; j < n - 1; ++j){
-            #pragma acc loop reduction(max : error)
-            for (int i {1}; i < m - 1; ++i){
-                Anew[j][i] = 0.25 * (A[j][i + 1] + A[j][i - 1] + A[j - 1][i] + A[j + 1][i]);
-                error = fmax(error, fabs(Anew[j][i] - A[j][i]));
+            #pragma acc parallel loop  present(A[0:n][0:m]) // collapse(2) reduction(max : error)
+            for (int j {1}; j < n - 1; ++j){
+                #pragma acc loop// reduction(max : error)
+                for (int i {1}; i < m - 1; ++i){
+                    Anew[j][i] = 0.25 * (A[j][i + 1] + A[j][i - 1] + A[j - 1][i] + A[j + 1][i]);
+                    error = MAX(error, fabs(Anew[j][i] - A[j][i]));
+                }
             }
-        }
 
-        #pragma acc parallel loop
-        for (int j {1}; j < n - 1; ++j){
-            #pragma acc loop
-            for (int i {1}; i < m - 1; ++i){
-                A[j][i] = Anew[j][i];
+            //#pragma acc parallel loop collapse(2)//мб коллапс?
+            for (int j {1}; j < n - 1; ++j){
+                //#pragma acc loop
+                for (int i {1}; i < m - 1; ++i){
+                    A[j][i] = Anew[j][i];
+                }
             }
-        }
 
-        if (iter % 100 == 0){
-            printf("%5d, %0.6f\n", iter, error);
+            /*if (iter % 100 == 0){
+                printf("%5d, %0.6f\n", iter, error);
+            }*/
+            ++iter;
+            #pragma update host(error)
         }
-        iter++;
-    }
+    #pragma acc exit data delete(A [0:n] [0:m],error,Anew[n][m])
 
     std::cout<<"Iterations: "<<iter<<std::endl<<"Error: "<<error<<std::endl;
+
 
     for (int i {0}; i < m; i++){
         delete[] A[i];
